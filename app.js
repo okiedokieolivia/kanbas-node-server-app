3b70c13b013d76bae5efe8d2eb1a8cff2b9882cd
Yifan He <oliviaaahe@yahoo.com>
2023-12-04 16:05:47 -0800

    Assignment6

--- app.js
+++ app.js
@@ -6,17 +6,47 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+console.log(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(express.json());
 app.use(
   cors({
+    credentials: true,
     origin: process.env.FRONTEND_URL,
   })
 );
+// const sessionOptions = {
+//   secret: "any string",
+//   resave: false,
+//   saveUninitialized: false,
+// };
+// app.use(session(sessionOptions));
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
 Lab5(app);
 Hello(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
